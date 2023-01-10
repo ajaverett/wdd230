e88e2ff01aeef70ed8051e667ca553a492e2f522
Aj! <88217322+ajaverett@users.noreply.github.com>
2023-01-10 10:41:43 -0700

    bug1 fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
 const last_update = new Date(document.lastModified);
-document.querySelector("#last_updated").innerHTML = last_update.getFullYear();
+document.querySelector("#current_year").innerHTML = last_update.getFullYear();
+
+
+// current_year
+const today = new Date()
+const dateOptions = {
+    year: 'numeric',
+    month: 'numeric', 
+    day: 'numeric'
+}
+
+document.querySelector('#last_updated').textContent = today.toLocaleDateString('en-US', dateOptions);
