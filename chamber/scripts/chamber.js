2283f18c39153b9cfb16fdb4a8ee2dabb6328060
Aj! <88217322+ajaverett@users.noreply.github.com>
2023-01-27 23:46:53 -0700

    fixed footer

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -25,3 +25,11 @@ window.addEventListener("resize", function() {
     };
   }
 });
+
+document.getElementById("last_updated").innerHTML = "Last Updated: " + new Date(document.lastModified).toLocaleString() + "...  WDD230";
+
+var d = new Date();
+var year = d.getFullYear();
+document.getElementById("current_year").innerHTML = year;
+
+console.log(document.lastModified)
