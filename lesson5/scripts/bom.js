0488c6b19ecb7a69590baaccf0494ed44112fb53
Aj! <88217322+ajaverett@users.noreply.github.com>
2023-02-02 10:11:19 -0700

    dom manip

--- lesson5/scripts/bom.js
+++ lesson5/scripts/bom.js
@@ -1,27 +1,36 @@
+const chapterInput = document.querySelector("#favchap");
+const addButton = document.querySelector("main button");
+const chapterList = document.querySelector("#list");
 
-const chapterInput = document.querySelector('#favchap');
-const addButton = document.querySelector('main button');
-const chapterList = document.querySelector('#list');
-
-addButton.addEventListener('click', () => {
-
+function chapterManipulation() {
     
-if (chapterInput.value == ''){
-    return;
-}
+    if (chapterInput.value == ""){
+        return;
+    }
 
-let newListItem = document.createElement('li');
-let deleteButton = document.createElement('button');
+    let newListItem = document.createElement("li");
+    let deleteButton = document.createElement("button");
 
-newListItem.textContent = chapterInput.value;
-deleteButton.textContent = '❌';
-newListItem.appendChild(deleteButton);
-chapterList.appendChild(newListItem);
+    newListItem.textContent = chapterInput.value;
+    deleteButton.textContent = '❌';
+    newListItem.appendChild(deleteButton);
+    chapterList.appendChild(newListItem);
 
-deleteButton.addEventListener('click', ()=>{
-    newListItem.remove()
-})
+    deleteButton.addEventListener('click', () => {
+        newListItem.remove();
+    });
 
-chapterInput.focus();
+    chapterInput.focus();
+    chapterInput.value = ""; 
+  }
 
-})
+addButton.addEventListener('click', chapterManipulation);
+
+addButton.addEventListener('click', chapterManipulation);
+
+chapterInput.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        chapterManipulation()
+    }
+});
+
