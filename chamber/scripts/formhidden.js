709073b6b46705d36b36ba1a7d17cc69c361ac57
Aj! <88217322+ajaverett@users.noreply.github.com>
2023-02-28 10:59:11 -0700

    bug

--- chamber/scripts/formhidden.js
+++ chamber/scripts/formhidden.js
@@ -1,3 +1,12 @@
 var now = new Date();
 var datetimeString = now.toLocaleDateString() + " " + now.toLocaleTimeString();
 document.getElementById("datetime_now").value = datetimeString;
+
+
+document.getElementById("last_updated").innerHTML = "Last Updated: " + new Date(document.lastModified).toLocaleString() + "...  WDD230";
+
+var d = new Date();
+var year = d.getFullYear();
+document.getElementById("current_year").innerHTML = year;
+
+
